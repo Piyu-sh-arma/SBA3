c5bf67c1aabbd25403672061457b53c835a1068b
newGitBee <piyu_sh_arma@hotmail.com>
2020-10-09 21:37:34 +0530

    Template for SBA3

---
+++ src/test/java/com/FSD/ITS/comFSDITS/InterviewTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FSD.ITS.comFSDITS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
