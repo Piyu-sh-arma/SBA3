2ee5cf9b1e07e8f1e531703fa133d1610849d405
newGitBee <piyu_sh_arma@hotmail.com>
2020-10-10 19:54:51 +0530

    validations added for user

--- src/main/java/com/FSD/ITS/validator/UserValidator.java
+++ src/main/java/com/FSD/ITS/validator/UserValidator.java
@@ -19,38 +19,38 @@ public class UserValidator {
                 } else
                     errors.add("First Name can't be blank.");
             } else
-                errors.add("First Name can't be null.");
+                errors.add("First Name is required.");
 
             if (null != user.getlName()) {
                 int lNameLen = user.getlName().trim().length();
                 if (lNameLen > 0) {
                     if (lNameLen < 3 || lNameLen > 25)
                         errors.add("Last name length should be between 3 to 25 Chars.");
-                }
-                errors.add("Last Name can't be blank.");
+                } else
+                    errors.add("Last Name can't be blank.");
             } else
-                errors.add("Last Name can't be null.");
+                errors.add("Last Name is required.");
 
 
             if (null != user.getEmail()) {
                 int emailLen = user.getEmail().trim().length();
                 if (emailLen > 0) {
-                    if (user.getEmail().matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"))
+                    if (!user.getEmail().matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"))
                         errors.add("Email:" + user.getEmail() + " is invalid.");
                 } else
                     errors.add("email can't be blank.");
             } else
-                errors.add("email can't be null.");
+                errors.add("email is required.");
 
 
             if (null != user.getMobile()) {
-                if (user.getMobile().matches("^\\d{10}$"))
+                if (!user.getMobile().matches("^\\d{10}$"))
                     errors.add("Mobile:" + user.getMobile() + " is invalid.");
             } else
-                errors.add("mobile can't be null or blank.");
+                errors.add("mobile is required.");
 
         } else
-            errors.add("User can't be null");
+            errors.add("User is required");
 
         return !(errors.size() > 0);
     }
